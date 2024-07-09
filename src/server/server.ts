388934c20a0c3d738cfff1034fd605041ea9d77e
Huon <huonswales@gmail.com>
2024-07-09 11:35:23 +1000

    Added deployment code

---
+++ src/server/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import helmet from "helmet";
+import compression from "compression";
+
+const app = express();
+app.use(helmet());
+app.use(compression());
+
+const frontendFiles = process.cwd() + "/dist";
+app.use(express.static(frontendFiles));
+app.get("/*", (_, res) => res.sendFile(frontendFiles + "/index.html"));
+
+app.listen(8000, () => console.log("Listening at http://localhost:8000/api/"));
